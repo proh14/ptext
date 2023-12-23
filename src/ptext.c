de884a53180a996ec2d8b0eab11bc4aa5bb2e190
Hoorad <hourrad.f@gmail.com>
2023-12-23 18:14:39 +1300

    Changed the license

--- src/ptext.c
+++ src/ptext.c
@@ -1,18 +1,3 @@
-/* Copyright (c) 2023 proh14. All rights reserved.
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by the Free
- *   Software Foundation, either version 3 of the License, or (at your option)
- *   any later version.
- *
- *   This program is distributed in the hope that it will be useful, but WITHOUT
- *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- *  You should have received a copy of the GNU General Public License along with
- *  this program. If not, see <https://www.gnu.org/licenses/>.
- */
 #define _DEFAULT_SOURCE
 #define _BSD_SOURCE
 #define _GNU_SOURCE
@@ -64,7 +49,7 @@ enum editorKeys {
   DEL_KEY,
 };
 
-struct {
+struct config {
   int cx, cy;
   int rx;
   row *rows;
@@ -79,7 +64,9 @@ struct {
   char statusmsg[80];
   time_t statusmsg_time;
   struct termios orig_termios;
-} conf;
+};
+
+struct config conf;
 
 #define INIT_BUFF                                                              \
   { NULL, 0 }
