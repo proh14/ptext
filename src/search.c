016b54c56bbb13f9323653fbf979025f9e567e3f
Hoorad <hourrad.f@gmail.com>
2023-12-25 20:14:37 +1300

    Added a way to restore the cursor after search

--- src/search.c
+++ src/search.c
@@ -22,6 +22,15 @@ void searchCallBack(char *query, int c) {
 }
 
 void search(void) {
+  int saved_cx = conf.cx;
+  int saved_cy = conf.cy;
+  int saved_rowoff = conf.rowoff;
   char *query = getPrompt("Search: %s", &searchCallBack);
-  free(query);
+  if (query) {
+    free(query);
+  } else {
+    conf.cx = saved_cx;
+    conf.cy = saved_cy;
+    conf.rowoff = saved_rowoff;
+  }
 }
