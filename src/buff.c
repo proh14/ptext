dd0238c79a9f2d526a3706e3b2a56a9f34e6aa62
Hoorad <hourrad.f@gmail.com>
2023-12-28 18:05:47 +1300

    Removed some warnings from buff.c

--- src/buff.c
+++ src/buff.c
@@ -2,8 +2,9 @@
 
 void buffAppend(struct buff *buff, const char *s, size_t len) {
   char *new = realloc(buff->chars, buff->len + len);
-  if (new == NULL)
+  if (new == NULL) {
     return;
+  }
   memcpy(&new[buff->len], s, len);
   buff->chars = new;
   buff->len += len;
