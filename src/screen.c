2b9d75d9290c92a45cc13432b2b0636f4066ceee
Hoorad Farrokh <hourrad.f@gmail.com>
2024-03-31 16:27:40 +1300

    Removed useless if statement

--- src/screen.c
+++ src/screen.c
@@ -3,9 +3,6 @@
 
 void screenAppend(struct screenBuffer *buff, const char *s, size_t len) {
   char *new = xrealloc(buff->chars, buff->len + len);
-  if (new == NULL) {
-    return;
-  }
   memcpy(&new[buff->len], s, len);
   buff->chars = new;
   buff->len += len;
